c04422692189194877238078ee49c370c9e41058
WerdnaH <huzepengyx@gmail.com>
2020-05-29 09:10:58 -0400

    break into tree.h

---
+++ fuzz.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+string generationalFuzz(int row, int col);
+string generationalFuzz(int row, int col) {
+    
+}
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    cout << Size << endl;
+    ::vector<vector<>>
+
+    //DoSomethingInterestingWithMyAPI(Data, Size);
+    return 0;  // Non-zero return values are reserved for future use.
+}
--- main.cpp
+++ main.cpp
@@ -3,28 +3,10 @@
 #include <sstream>
 #include <vector>
 #include <map>
+
+#include "tree.h"
+
 using namespace std;
-
-class Token {
-    public:
-    string player;
-    string height;
-    Token() = default;
-    Token(string p, string h) {
-        player = p;
-        height = h;
-    }
-};
-
-
-vector<vector<Token>> readInput(string input);
-
-
-map<string, int> score(vector<vector<Token>> board, string direct);
-
-
-bool isBlocked(vector<vector<Token>> board, int i, int j, int dx, int dy, map<string, int>);
-
 
 int main() {
     vector<vector<Token>> res = readInput(
@@ -34,7 +16,7 @@ int main() {
         " _, _,Red_S,Blue_T,Red_T;"
         " _,Red_S,Blue_S, _, _;"
     );
-    cout << res.size() <<endl;
+    cout << res.size() << endl;
     for (int i = 0; i < res.size(); i++) {
         for (int j = 0; j < res[i].size(); j++) {
             cout << res[i][j].player << res[i][j].height << i << j << endl;
@@ -44,89 +26,6 @@ int main() {
     for (auto e : result) {
         cout << e.first << e.second << endl;
     }
-}
 
-
-
-vector<vector<Token>> readInput(string input) {
-    vector<vector<Token>> ret;
-    stringstream ss(input);
-    string tokens;
-    while (getline(ss, tokens, ';')) {
-        vector<Token> row;
-        stringstream ss_inner(tokens);
-        string token;
-        cout << tokens << endl;
-        while (getline(ss_inner, token, ',')) {
-            row.push_back(Token(
-                token.substr(0, token.find('_')), 
-                token.substr(token.find('_') + 1))
-            );
-            //cout << token << row.back().height << endl;
-        }
-        ret.push_back(row);
-    }
-    return ret;
-}
-
-map<string, int> score(vector<vector<Token>> board, string direct) {
-    map<string, int> record;
-    map<string, int> order;
-    order["S"] = 1;
-    order["T"] = 2;
-    order["V"] = 3;
-    int dx = 0;
-    int dy = 0;
-    if (direct == "N") {
-        dx = 1;
-    } else if (direct == "S") {
-        dx = -1;
-    } else if (direct == "E") {
-        dy = 1;
-    } else {    
-        dy = -1;
-    }
-
-    for (int i = 0; i < board.size(); i++) {
-        for (int j = 0; j < board[0].size(); j++) {
-            string name = board[i][j].player;
-            cout << i << j << name << board[i][j].height << "============" << endl;
-            if (name != " " && !isBlocked(board, i, j ,dx, dy, order))  {
-                cout << "score" << i << j << endl;
-                if (!record.count(name)) {
-                    record[name] = 0;
-                }
-                if (board[i][j].height == "S") {
-                    record[name]++;
-                } else if (board[i][j].height == "T") {
-                    record[name] += 2;
-                } else {
-                    record[name] += 3;
-                }
-            }
-        }
-    }
-    return record;
-}
-
-
-bool isBlocked(vector<vector<Token>> board, int i, int j, int dx, int dy, map<string, int> order) {
-    int oriOrder = order[board[i][j].height];
-    for (int h = 1; h <= 3; h++) {
-        i += dx;
-        j += dy;
-        cout << i << j << "|" << board[i][j].height << "|" << h << endl;
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size()) {
-            return false;
-        }
-        int curOrder = order[board[i][j].height];
-        if (h == 1 && curOrder >= oriOrder) {
-            return true;
-        } else if (h == 2 && curOrder >= oriOrder && curOrder >= 2) {
-            return true;
-        } else if (h == 3 && curOrder >= oriOrder && curOrder >= 3) {
-            return true;
-        }
-    }
-    return false;
+    printf("=======\n");
 }
